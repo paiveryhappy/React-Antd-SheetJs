93910e6d7eacebcde56570372d67d34415697daf
Jiran <paiveryhappy@gmail.com>
2024-05-11 19:14:22 +0700

    feat:add next/font

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,9 @@
-'use client'
-import React, { useState } from 'react';
-import { Tabs, Input } from 'antd';
-import TableData from "../components/TableData"
-import XLSX from "xlsx";
+"use client";
+import React, { useState } from "react";
+import { Tabs, Input } from "antd";
+import TableData from "../components/TableData";
 
 export default function Home() {
-
   const [searchWriter, setSearchWriter] = useState("");
   const [searchCover, setSearchCover] = useState("");
   const [searchCategory, setSearchCategory] = useState("");
@@ -13,16 +11,16 @@ export default function Home() {
 
   const setWriterInput = (newValue) => {
     setSearchWriter(newValue);
-  }
+  };
   const setCoverInput = (newValue) => {
     setSearchCover(newValue);
-  }
+  };
   const setCategoryInput = (newValue) => {
     setSearchCategory(newValue);
-  }
+  };
   const setCustomerInput = (newValue) => {
     setSearchCustomer(newValue);
-  }
+  };
   return (
     <>
       <main className="container">
@@ -30,31 +28,47 @@ export default function Home() {
           <h2 className="text-xl font-bold">Article</h2>
           <div className="grid-container mt-2">
             <div>
-              <label htmlFor="writer" className="font-bold">Writer: </label>
-              <Input id="writer"
+              <label htmlFor="writer" className="font-bold">
+                Writer:{" "}
+              </label>
+              <Input
+                id="writer"
                 onChange={(e) => setSearchWriter(e.target.value)}
-                value={searchWriter} />
+                value={searchWriter}
+              />
             </div>
 
             <div>
-              <label htmlFor="cover" className="font-bold">Cover: </label>
-              <Input id="cover"
+              <label htmlFor="cover" className="font-bold">
+                Cover:{" "}
+              </label>
+              <Input
+                id="cover"
                 onChange={(e) => setSearchCover(e.target.value)}
-                value={searchCover} />
+                value={searchCover}
+              />
             </div>
 
             <div>
-              <label htmlFor="CategoryType" className="font-bold">Category: </label>
-              <Input id="CategoryType"
+              <label htmlFor="CategoryType" className="font-bold">
+                Category:{" "}
+              </label>
+              <Input
+                id="CategoryType"
                 onChange={(e) => setSearchCategory(e.target.value)}
-                value={searchCategory} />
+                value={searchCategory}
+              />
             </div>
 
             <div>
-              <label htmlFor="customer" className="font-bold">Customer: </label>
-              <Input id="customer"
+              <label htmlFor="customer" className="font-bold">
+                Customer:{" "}
+              </label>
+              <Input
+                id="customer"
                 onChange={(e) => setSearchCustomer(e.target.value)}
-                value={searchCustomer} />
+                value={searchCustomer}
+              />
             </div>
           </div>
         </header>
@@ -66,23 +80,24 @@ export default function Home() {
             {
               label: "Article Detail",
               key: "1",
-              children: <TableData
-                searchWriter={searchWriter}
-                searchCustomer={searchCustomer}
-                searchCover={searchCover}
-                searchCategory={searchCategory}
-                changeWriterInput={setWriterInput}
-                changeCoverInput={setCoverInput}
-                changeCategoryInput={setCategoryInput}
-                changeCustomerInput={setCustomerInput} />,
+              children: (
+                <TableData
+                  searchWriter={searchWriter}
+                  searchCustomer={searchCustomer}
+                  searchCover={searchCover}
+                  searchCategory={searchCategory}
+                  changeWriterInput={setWriterInput}
+                  changeCoverInput={setCoverInput}
+                  changeCategoryInput={setCategoryInput}
+                  changeCustomerInput={setCustomerInput}
+                />
+              ),
             },
           ]}
-
         />
       </main>
       <br />
       <br />
-
     </>
   );
 }
